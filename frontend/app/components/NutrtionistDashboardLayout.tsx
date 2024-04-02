5fcdd933fbaec60cc023e6b9b45196ea92eae071
Lucky Victory <LuckyVictory54@gmail.com>
2024-04-02 14:42:21 +0100

    feat: :sparkles: Made dashboard sidebar mobile responsive

--- frontend/app/components/NutrtionistDashboardLayout.tsx
+++ frontend/app/components/NutrtionistDashboardLayout.tsx
@@ -9,34 +9,47 @@ import {
   MdOutlineSettings,
   MdOutlineDashboard,
 } from "react-icons/md";
+import {
+  HiOutlineCalendarDays,
+  HiOutlineDocumentText,
+  HiOutlineSquares2X2,
+  HiOutlineCog6Tooth,
+  HiOutlinePresentationChartLine,
+  HiOutlineLightBulb,
+} from "react-icons/hi2";
 export default function DashboardLayout({ children }: { children: ReactNode }) {
   const navLinks = [
-    { url: "overview", title: "Overview", icon: MdOutlineDashboard, child: [] },
+    {
+      url: "overview",
+      title: "Overview",
+      icon: HiOutlineSquares2X2,
+      child: [],
+    },
     {
       url: "meal-plans",
       title: "Meal Plans",
-      icon: MdOutlineFastfood,
-      child: ["new"],
-    },
-    {
-      url: "fitness-plans",
-      title: "Fitness Plans",
-      icon: MdOutlineFastfood,
-      child: ["new"],
-    },
-    {
-      url: "articles",
-      title: "Articles",
-      icon: MdOutlinePostAdd,
+      icon: HiOutlinePresentationChartLine,
       child: ["new"],
     },
     {
       url: "appointments",
       title: "Appointments",
-      icon: MdOutlineFastfood,
+      icon: HiOutlineCalendarDays,
       child: [],
     },
-    { url: "settings", title: "Settings", icon: MdOutlineSettings, child: [] },
+    {
+      url: "fitness-plans",
+      title: "Fitness Plans",
+      icon: HiOutlineLightBulb,
+      child: ["new"],
+    },
+    {
+      url: "articles",
+      title: "Articles",
+      icon: HiOutlineDocumentText,
+      child: ["new"],
+    },
+    { url: "settings", title: "Settings", icon: HiOutlineCog6Tooth, child: [] },
   ];
   return (
     <PageLoader>

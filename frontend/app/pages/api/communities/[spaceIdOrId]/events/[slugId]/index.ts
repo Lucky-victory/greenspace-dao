80d67600463133b5066e81af6968d5453a790f10
Lucky Victory <LuckyVictory54@gmail.com>
2024-04-04 14:24:19 +0100

    feat: :sparkles: completed community APIs

---
+++ frontend/app/pages/api/communities/[spaceIdOrId]/events/[slugId]/index.ts
@@ -0,0 +1,77 @@
+import { db } from "@/db";
+import { communityEvents } from "@/db/schema";
+import {
+  HTTP_METHOD_CB,
+  errorHandlerCallback,
+  mainHandler,
+  successHandlerCallback,
+} from "@/utils";
+import { eq, or } from "drizzle-orm";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  return mainHandler(req, res, {
+    GET,
+  });
+}
+
+export const GET: HTTP_METHOD_CB = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const { slugId } = req.query as { slugId: string };
+
+    const challenge = await db.query.communityEvents.findFirst({
+      where: or(eq(communityEvents.slugId, slugId)),
+      with: {
+        tags: {
+          columns: {
+            createdAt: false,
+            updatedAt: false,
+          },
+        },
+      },
+    });
+
+    return successHandlerCallback(req, res, {
+      message: "challenge retrieved successfully",
+      data: challenge,
+    });
+  } catch (error) {
+    return errorHandlerCallback(req, res, {
+      message: "Something went wrong...",
+      data: null,
+    });
+  }
+};
+
+// export const POST: HTTP_METHOD_CB = async (
+//   req: NextApiRequest,
+//   res: NextApiResponse
+// ) => {
+//   try {
+//     const data = req.body;
+
+//     const createdRecord = await db.transaction(async (tx) => {
+//       const [insertRes] = await tx.insert(communityEvents).values(data);
+
+//       return tx.query.communityEvents.findFirst({
+//         where: eq(communityEvents.id, insertRes.insertId),
+//       });
+//     });
+//     return successHandlerCallback(req, res, {
+//       message: "Event added successfully",
+//       data: createdRecord,
+//     });
+//   } catch (error) {
+//     return errorHandlerCallback(req, res, {
+//       message: "Something went wrong...",
+//       data: null,
+//       error,
+//     });
+//   }
+// };

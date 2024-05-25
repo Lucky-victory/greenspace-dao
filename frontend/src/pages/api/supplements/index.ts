d1c9f9567854f409bb02179404e047eb9092939f
saqlainprinters <saqlainprinters@gmail.com>
2024-05-25 11:11:37 +0000

    feat: supplements apis

--- frontend/src/pages/api/supplements/index.ts
+++ frontend/src/pages/api/supplements/index.ts
@@ -7,26 +7,108 @@ import { HTTP_METHOD_CB, errorHandlerCallback, mainHandler, successHandlerCallba
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   return mainHandler(req, res, {
     GET,
-    //   POST,
+    POST,
+    DELETE,
   });
 }
 
 export const GET: HTTP_METHOD_CB = async (req, res) => {
   try {
-    const supplementsWithDetails = await db
-      .select({
-        supplement: supplements,
-        attributes: supplementAttributes,
-        recommendations: supplementRecommendations,
-        experts: supplementExperts,
-      })
-      .from(supplements)
-      .leftJoin(supplementAttributes, eq(supplementAttributes.supplementId, supplements.id))
-      .leftJoin(supplementRecommendations, eq(supplementRecommendations.supplementId, supplements.id))
-      .leftJoin(supplementExperts, eq(supplementExperts.id, supplementRecommendations.expertId));
+    const allSupplements = await db.select().from(supplements);
+    const allSupplementsAttributes = await db.select().from(supplementAttributes);
+    const allSupplementsRecommendations = await db.select().from(supplementRecommendations);
+    const allSupplementsExperts = await db.select().from(supplementExperts);
+    const data = allSupplements.reduce((acc, curr) => {
+      const supplementId = curr.id;
+      const attributes = allSupplementsAttributes
+        .filter((item) => item.supplementId === supplementId)
+        .map((item) => item.attribute);
+      let recommendations = allSupplementsRecommendations
+        .filter((item) => item.supplementId === supplementId)
+        .map((item) => ({ expertId: item.expertId, doseage: item.doseage }));
+      const experts = allSupplementsExperts.filter((item) => recommendations.some((rec) => rec.expertId === item.id));
+
+      recommendations = recommendations.map((item) => {
+        // @ts-ignore
+        const expert = experts.find((expert) => expert.id === item.expertId);
+        return { ...item, ...expert };
+      });
+      // @ts-ignore
+      acc.push({ ...curr, attributes, recommendations });
+      return acc;
+    }, []);
     return successHandlerCallback(req, res, {
       message: "supplements retrieved successfully",
-      data: supplementsWithDetails || null,
+      data,
+    });
+  } catch (error) {
+    return errorHandlerCallback(req, res, {
+      message: "Something went wrong...",
+      data: null,
+      error,
+    });
+  }
+};
+interface SupplimentBody {
+  title: string;
+  intro: string;
+  link: string;
+  attributes: string[];
+  recommendations: { expertId: number; doseage: string }[];
+}
+export const POST: HTTP_METHOD_CB = async (req, res) => {
+  try {
+    const { title, intro, link, attributes, recommendations } = req.body as SupplimentBody;
+    const supplementExists = await db.query.supplements.findFirst({
+      where: eq(supplements.title, title),
+    });
+    if (supplementExists) {
+      return successHandlerCallback(req, res, {
+        message: "supplement already exists",
+        data: supplementExists,
+      });
+    }
+    const [{ insertId: supplementId }] = await db.insert(supplements).values({
+      title,
+      intro,
+      link,
+    });
+
+    await db.insert(supplementAttributes).values(
+      attributes.map((item) => ({
+        supplementId,
+        attribute: item,
+      }))
+    );
+
+    await db.insert(supplementRecommendations).values(
+      recommendations.map((item) => ({
+        supplementId,
+        expertId: item.expertId,
+        doseage: item.doseage,
+      }))
+    );
+
+    return successHandlerCallback(req, res, {
+      message: "supplement created successfully",
+      data: { supplementId },
+    });
+  } catch (error) {
+    return errorHandlerCallback(req, res, {
+      message: "Something went wrong...",
+      data: null,
+      error,
+    });
+  }
+};
+
+export const DELETE: HTTP_METHOD_CB = async (req, res) => {
+  try {
+    const { supplementId } = req.body;
+    await db.delete(supplements).where(eq(supplements.id, supplementId));
+    return successHandlerCallback(req, res, {
+      message: "supplement deleted successfully",
+      data: { supplementId },
     });
   } catch (error) {
     return errorHandlerCallback(req, res, {

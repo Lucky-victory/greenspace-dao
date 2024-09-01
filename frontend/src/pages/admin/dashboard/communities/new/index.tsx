3067b7ffc15fa0f7a110f1dd29cf6abb7cd2fc3c
Lucky Victory <luckyvictory54@gmail.com>
2024-09-01 16:34:48 +0100

    bug fix at admin dashboard

--- frontend/src/pages/admin/dashboard/communities/new/index.tsx
+++ frontend/src/pages/admin/dashboard/communities/new/index.tsx
@@ -17,7 +17,7 @@ import DashboardLayout from "src/components/AdminDashboardLayout";
 import TextEditor from "src/components/TextEditor";
 
 import { useState } from "react";
-import CoverImageUploader from "src/components/CommunityPage/TabPanels/CoverImageUploader";
+import CoverImageUploader from "src/components/CoverImageUploader";
 import { useStorageUpload } from "@thirdweb-dev/react";
 import { useFormik } from "formik";
 import { resolveIPFSURI } from "src/helpers";
